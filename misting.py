e544d17bfc69a1a10fe2036b4ae8886aa4d7936a
roshan922006 <roshan922006@gmail.com>
2025-04-19 00:32:18 +0530

    misting.py

---
+++ misting.py
@@ -0,0 +1,6 @@
+def activate_misting():
+    print("Misting system activated. Cleaning solar panel...")
+
+    # Logic to activate misting nozzles
+    # For instance, send a signal to a relay controlling the misting system
+    # relay.activate_misting()
